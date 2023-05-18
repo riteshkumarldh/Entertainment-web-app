ee7cbd3e5adf3fc2de2dd6877b461541f2e4b1c9
Ritesh <ritesh.comforttechno@gmail.com>
2023-05-18 22:13:38 +0530

    bookmark page done

--- src/hook/useBookmark.js
+++
@@ -1,5 +0,0 @@
-import { useSelector } from "react-redux";
-
-const s = useSelector((state) => state.bookmark.bookmarked);
-
-console.log(s);
--- src/pages/Bookmark.jsx
+++ src/pages/Bookmark.jsx
@@ -1,3 +1,25 @@
+import { useSelector } from "react-redux";
+import SingleRecommended from "../components/SingleRecommended";
+import { Link } from "react-router-dom";
+
 export default function Bookmark() {
-  return <section className="main">Ritesh bookmarked</section>;
+  const bookmarked = useSelector((state) => state.bookmark.bookmarked);
+
+  return (
+    <section className="main trending bookmarked">
+      <h3 className="trending__title">Recommended For You</h3>
+      <div className="recommended__container">
+        {bookmarked.length > 0 ? (
+          bookmarked.map((data) => {
+            return <SingleRecommended key={data.id} movie={data} />;
+          })
+        ) : (
+          <div className="no-bookmark">
+            <h2>You have not saved any Movie or TV</h2>
+            <Link to="/">Go to Home</Link>
+          </div>
+        )}
+      </div>
+    </section>
+  );
 }
